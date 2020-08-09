ffb413667f347eea25f55ba8c848cde031e5bc1c
Edgard Oliveira <eafo.hef2@uea.edu.br>
2020-08-08 20:43:10 -0400

    envio inicial. Room (Crud funcionando)

---
+++ app/src/main/java/com/technologies/venom/room/models/Endereco.java
@@ -0,0 +1,103 @@
+package com.technologies.venom.room.models;
+
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "enderecos")
+public class Endereco {
+
+    @PrimaryKey(autoGenerate = true)
+    private long enderecoId;
+    private String cep;
+    private String logradouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cidade;
+    private String uf;
+
+    //construtor vazio
+    @Ignore
+    public Endereco() {
+
+    }
+
+    //construtor com parâmetros
+    public Endereco(String cep, String logradouro, String numero, String complemento, String bairro, String cidade, String uf) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+    }
+
+    //getters
+    public long getEnderecoId() {
+        return enderecoId;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+
+    //setters
+    public void setEnderecoId(long enderecoId) {
+        this.enderecoId = enderecoId;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+}
